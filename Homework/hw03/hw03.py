a1cd8b9efafde1f2a60e8ac3f52c95e0b1a61c1e
xkz0777 <821376043@qq.com>
2021-12-28 15:25:27 +0800

    update hw03 and lab03, remove redundant files

---
+++ Homework/hw03/hw03.py
@@ -0,0 +1,218 @@
+HW_SOURCE_FILE = __file__
+from math import log2
+
+
+def composer(func=lambda x: x):
+    """
+    Returns two functions -
+    one holding the composed function so far, and another
+    that can create further composed problems.
+    >>> add_one = lambda x: x + 1
+    >>> mul_two = lambda x: x * 2
+    >>> f, func_adder = composer()
+    >>> f1, func_adder = func_adder(add_one)
+    >>> f1(3)
+    4
+    >>> f2, func_adder = func_adder(mul_two)
+    >>> f2(3) # should be 1 + (2*3) = 7
+    7
+    >>> f3, func_adder = func_adder(add_one)
+    >>> f3(3) # should be 1 + (2 * (3 + 1)) = 9
+    9
+    """
+    def func_adder(g):
+        "*** YOUR CODE HERE ***"
+        return composer(lambda x: func(g(x)))
+
+    # Obviously, func_adder should return the result after calling composer, so we need to think about its argument.
+    # It should be a function, which takes in an argument x, return the composition.
+    # Notice that the function passed in can not be func(g), because the argument of func shall not be callable
+    return func, func_adder
+
+
+def g(n):
+    """Return the value of G(n), computed recursively.
+
+    >>> g(1)
+    1
+    >>> g(2)
+    2
+    >>> g(3)
+    3
+    >>> g(4)
+    10
+    >>> g(5)
+    22
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'g', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n <= 3:
+        return n
+    return g(n - 1) + 2 * g(n - 2) + 3 * g(n - 3)
+
+
+def g_iter(n):
+    """Return the value of G(n), computed iteratively.
+
+    >>> g_iter(1)
+    1
+    >>> g_iter(2)
+    2
+    >>> g_iter(3)
+    3
+    >>> g_iter(4)
+    10
+    >>> g_iter(5)
+    22
+    >>> from construct_check import check
+    >>> # ban recursion
+    >>> check(HW_SOURCE_FILE, 'g_iter', ['Recursion'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n <= 3:
+        return n
+    a, b, c = 1, 2, 3
+    while n > 3:
+        a, b, c, n = b, c, 3 * a + 2 * b + c, n - 1
+    return c
+
+
+def missing_digits(n):
+    """Given a number a that is in sorted, increasing order,
+    return the number of missing digits in n. A missing digit is
+    a number between the first and last digit of a that is not in n.
+    >>> missing_digits(1248) # 3, 5, 6, 7
+    4
+    >>> missing_digits(1122) # No missing numbers
+    0
+    >>> missing_digits(123456) # No missing numbers
+    0
+    >>> missing_digits(3558) # 4, 6, 7
+    3
+    >>> missing_digits(35578) # 4, 6
+    2
+    >>> missing_digits(12456) # 3
+    1
+    >>> missing_digits(16789) # 2, 3, 4, 5
+    4
+    >>> missing_digits(19) # 2, 3, 4, 5, 6, 7, 8
+    7
+    >>> missing_digits(4) # No missing numbers between 4 and 4
+    0
+    >>> from construct_check import check
+    >>> # ban while or for loops
+    >>> check(HW_SOURCE_FILE, 'missing_digits', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n < 10:
+        return 0  # No missing numbers between n and n
+    all_but_last, last = divmod(n, 10)
+    diffrence = max(last - all_but_last % 10 - 1, 0)
+    return missing_digits(all_but_last) + diffrence
+
+
+def count_change(total):
+    """Return the number of ways to make change for total.
+
+    >>> count_change(7)
+    6
+    >>> count_change(10)
+    14
+    >>> count_change(20)
+    60
+    >>> count_change(100)
+    9828
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_change', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    mp = {}  # memoization
+
+    def cal_exponent(n):  # return the max exponent
+        if n <= 1:
+            return 0
+        else:
+            return cal_exponent(n >> 1) + 1
+
+    def helper(total, m):  # sum up to total using numbers up to m
+        if total == 0 or total == 1 or m == 1:
+            return 1
+        elif total < 0:
+            return 0
+        elif (total, m) in mp:
+            return mp[(total, m)]
+        else:
+            mp[(total, m)] = helper(total - m, m) + helper(total, m // 2)
+            return mp[(total, m)]
+
+    return helper(total, pow(2, cal_exponent(total)))
+
+
+def print_move(origin, destination):
+    """Print instructions to move a disk."""
+    print("Move the top disk from rod", origin, "to rod", destination)
+
+
+def move_stack(n, start, end):
+    """Print the moves required to move n disks on the start pole to the end
+    pole without violating the rules of Towers of Hanoi.
+
+    n -- number of disks
+    start -- a pole position, either 1, 2, or 3
+    end -- a pole position, either 1, 2, or 3
+
+    There are exactly three poles, and start and end must be different. Assume
+    that the start pole has at least n disks of increasing size, and the end
+    pole is either empty or has a top disk larger than the top n start disks.
+
+    >>> move_stack(1, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    >>> move_stack(2, 1, 3)
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 3
+    >>> move_stack(3, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 3 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 1
+    Move the top disk from rod 2 to rod 3
+    Move the top disk from rod 1 to rod 3
+    """
+    assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
+    "*** YOUR CODE HERE ***"
+    if n == 0:
+        return
+    transfer = 6 - start - end  # the third pod except start and end
+    move_stack(n - 1, start, transfer)
+    print_move(start, end)
+    move_stack(n - 1, transfer, end)
+
+
+from operator import sub, mul
+
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from construct_check import check
+    >>> # ban any assignments or recursion
+    >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
+    True
+    """
+    return (lambda fact: lambda x: fact(fact, x))(lambda fact, x: 1 if x <= 1 else mul(x, fact(fact, x - 1)))
+    # Let's divide it into two parts:
+    # First, we need the fact function to call itself, if we cannot give it a name, we can make it an argument.
+    # So the second part serve as a `fact` function.
+    # Second, since there are no arguments passed in, we need to apply that fact function inside `make_anonymous_factorial`,
+    # and the first part "(lambda fact: lambda x: fact(fact, x))" is doing this job
---
+++ Lab/lab03/lab03.py
@@ -0,0 +1,131 @@
+HW_SOURCE_FILE=__file__
+
+
+def pascal(row, column):
+    """Returns a number corresponding to the value at that location
+    in Pascal's Triangle.
+    >>> pascal(0, 0)
+    1
+    >>> pascal(0, 5)	# Empty entry; outside of Pascal's Triangle
+    0
+    >>> pascal(3, 2)	# Row 4 (1 3 3 1), 3rd entry
+    3
+    """
+    "*** YOUR CODE HERE ***"
+    if row < 0 or column < 0 or column > row:
+        return 0
+    elif column == row or column == 0:
+        return 1
+    return pascal(row - 1, column) + pascal(row - 1, column - 1)
+
+
+def compose1(f, g):
+    """"Return a function h, such that h(x) = f(g(x))."""
+    def h(x):
+        return f(g(x))
+    return h
+
+def repeated(f, n):
+    """Return the function that computes the nth application of func (recursively!).
+
+    >>> add_three = repeated(lambda x: x + 1, 3)
+    >>> add_three(5)
+    8
+    >>> square = lambda x: x ** 2
+    >>> repeated(square, 2)(5) # square(square(5))
+    625
+    >>> repeated(square, 4)(5) # square(square(square(square(5))))
+    152587890625
+    >>> repeated(square, 0)(5)
+    5
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'repeated',
+    ...       ['For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 0:
+        return lambda x: x
+    return compose1(f, repeated(f, n - 1))
+
+
+def num_eights(x):
+    """Returns the number of times 8 appears as a digit of x.
+
+    >>> num_eights(3)
+    0
+    >>> num_eights(8)
+    1
+    >>> num_eights(88888888)
+    8
+    >>> num_eights(2638)
+    1
+    >>> num_eights(86380)
+    2
+    >>> num_eights(12345)
+    0
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_eights',
+    ...       ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if x == 0:
+        return 0
+    return num_eights(x // 10) + (1 if x % 10 == 8 else 0)
+
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(8)
+    8
+    >>> pingpong(10)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    -2
+    >>> pingpong(30)
+    -2
+    >>> pingpong(68)
+    0
+    >>> pingpong(69)
+    -1
+    >>> pingpong(80)
+    0
+    >>> pingpong(81)
+    1
+    >>> pingpong(82)
+    0
+    >>> pingpong(100)
+    -6
+    >>> from construct_check import check
+    >>> # ban assignment statements
+    >>> check(HW_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def successor(n, direction):
+        '''
+        DIRECTION is 0 when counting up, 1 otherwise
+        return the successor of n
+        '''
+        return n - 1 if direction else n + 1
+
+    def switch(direction):
+        '''Switch diretion'''
+        return 0 if direction else 1
+
+    def search(value, index, target_index, direction):
+        if index == target_index:
+            return value
+        elif num_eights(index) or index % 8 == 0:
+            return search(successor(value, switch(direction)), index + 1, target_index, switch(direction))
+        else:
+            return search(successor(value, direction), index + 1, target_index, direction)
+    return search(1, 1, n, 0)
